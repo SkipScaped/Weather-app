c446413faed2943e715f766e019c3c4ac5e743d9
Aaliyan <159554008+SkipScaped@users.noreply.github.com>
2024-07-10 20:52:44 +0500

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios, {isCancel, AxiosError} from 'axios';
 async function getgeoloacationForCityName(cityName) {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=2b5fc755ac2ec59250868b5527df31c4`);
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=ecae0dec56487af50571e624d0a04dad`);
     return response
 
     } catch (error) {
@@ -10,7 +10,7 @@ async function getgeoloacationForCityName(cityName) {
   }
  export async function getWeatherInfo(lat, lon) {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=part&appid=2b5fc755ac2ec59250868b5527df31c4`);
+      const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=part&appid=ecae0dec56487af50571e624d0a04dad`);
       return response
     } catch (error) {
       console.error(error);
